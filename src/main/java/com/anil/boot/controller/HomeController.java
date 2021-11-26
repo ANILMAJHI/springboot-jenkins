a0a053f656e03fb027cb1c4361b080c4e7d06b21
AnilMajhi63 <anil.majhi63@gmail.com>
2021-11-26 17:33:36 +0530

    initial code commit

---
+++ src/main/java/com/anil/boot/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.anil.boot.controller;
+
+import org.apache.logging.log4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	private static Logger logger = (Logger) LoggerFactory.getLogger(HomeController.class);
+
+	
+	@GetMapping("/home")
+	public String HomeMessage()
+	{
+		logger.info("testing the jenkins is working or not");
+		return "spring-jenkins";
+	}
+}
