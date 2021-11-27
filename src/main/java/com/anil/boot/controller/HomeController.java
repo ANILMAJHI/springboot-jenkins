68a1fc7c96f8e9148e9e4a72d563754af0abef50
AnilMajhi63 <anil.majhi63@gmail.com>
2021-11-27 09:28:54 +0530

    dadddd

--- src/main/java/com/anil/boot/controller/HomeController.java
+++ src/main/java/com/anil/boot/controller/HomeController.java
@@ -13,10 +13,8 @@ public class HomeController {
 	public String HomeMessage()
 	{
 		//logger.info("testing the jenkins is working or not");
-<<<<<<< HEAD
+
 		return "spring-jenkins application added newly...adfasfsafasdfsa";
-=======
-		return "spring-jenkins added dev, UAT, PROD...new line added";
->>>>>>> 4309fffa414bc87acccfcc46022067d358c60c58
+
 	}
 }
